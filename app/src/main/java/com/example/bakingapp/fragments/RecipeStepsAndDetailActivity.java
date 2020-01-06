fdc6500fb543a80594a8005fcaf8dbf33ac4223c
Goran Novakovic <gnovakov@gmail.com>
2020-01-06 13:20:39 +0000

    Fragment Activities Created
    Fragment Activities:
    
    RecipeStepsAndDetailActivity & RecipeDetailActivity Created.

---
+++ app/src/main/java/com/example/bakingapp/fragments/RecipeStepsAndDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.bakingapp.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bakingapp.R;
+
+public class RecipeStepsAndDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_steps_and_detail);
+    }
+
+
+}
