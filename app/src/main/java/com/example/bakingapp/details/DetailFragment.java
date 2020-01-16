a2b8947d926253cdd6e7ec7f03551e15af4d6b5b
Goran Novakovic <gnovakov@gmail.com>
2020-01-16 17:31:33 +0000

    ExoPlayer Half Implemented
    End of Day Submit.

--- app/src/main/java/com/example/bakingapp/details/DetailFragment.java
+++ app/src/main/java/com/example/bakingapp/details/DetailFragment.java
@@ -1,16 +1,28 @@
 package com.example.bakingapp.details;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.bakingapp.R;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class DetailFragment extends Fragment {
 
@@ -22,12 +34,19 @@ public class DetailFragment extends Fragment {
 
 
 
+
+
     public DetailFragment() {
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        SimpleExoPlayer absPlayerInternal;
+        PlayerView pvMain;
+
+        String CONTENT_URL;
 
         View rootView = inflater.inflate(R.layout.fragment_details, container, false);
         TextView stepTitle = rootView.findViewById(R.id.stepTitle); // Find TextView id
@@ -46,7 +65,21 @@ public class DetailFragment extends Fragment {
             stepTitle.setText("Step " + id + ": " + shortDescription);
         }
 
+        // Set Video URL
+        if (videoURL.length() == 0) {
+            CONTENT_URL = thumbnailURL;
+        } else {
+            CONTENT_URL = videoURL;
+        }
+
         stepDescription.setText(description);
+
+        //Implement ExoPlayer
+        int appNameStringRes = R.string.app_name;
+
+        pvMain = findViewById(R.id.pv_main); // creating player view
+
+
 
         return rootView;
     }
